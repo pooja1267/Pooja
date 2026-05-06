a10ef74e8496255dce3a064156bb94abfbbb67fd
Pooja Mehndiratta <pooja236@gmail.com>
2026-05-06 12:03:47 +0530

    max

---
+++ Tutort/Assignments/DataTypeOperatorAndIfElse/Max3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter 3 numbers";
+    cin>>a>>b>>c;
+    int res = ((a>b)?a:b) > c ? ((a>b)?a:b) : c;
+    cout<<"Max is:"<<res<<endl;
+    return 0;
+}
