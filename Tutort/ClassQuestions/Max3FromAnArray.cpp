29966c0c9502b47bc098e6f4d9e9f50a5c68c60f
Pooja Mehndiratta <pooja236@gmail.com>
2026-05-09 09:48:43 +0530

    test

---
+++ Tutort/ClassQuestions/Max3FromAnArray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,1,2,8,8,1,6,7,4};
+    int first=0, second=0, third=0;
+    for(auto i:arr)
+    {
+        if(i>first)
+        {
+            third=second;
+            second=first;
+            first=i;
+        }
+        else if(i>second && i!=first)
+        {
+            third=second;
+            second=i;
+        }
+        else if(i>third && i!=second)
+        {
+            third=i;
+        }
+        
+
+    }
+
+    cout<<first<<"\t"<<second<<"\t"<<third<<endl;
+    return 0;
+}
